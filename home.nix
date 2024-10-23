4be7ab94338e915e451a8532f0356983ebc73982
Reyhan Kamil <36887379+ryhkml@users.noreply.github.com>
2024-10-23 15:57:29 +0000

    Update

--- home.nix
+++ home.nix
@@ -53,10 +53,10 @@ let
   # Bun version update in nixpkgs is lengthy
   bunBin = pkgs.stdenv.mkDerivation rec {
     pname = "bun";
-    version = "1.1.31";
+    version = "1.1.32";
     src = pkgs.fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-      sha256 = "16rlx9y7im6nbdjcz1ri2md55i70q8nsqijg5a1fidxdh8dssy6c";
+      sha256 = "1ig875izfp49gcavrryfi5mnhikq2n76f4mki9mw3j0wjab0l5z2";
     };
     nativeBuildInputs = [ pkgs.unzip ];
     phases = [ "unpackPhase" "installPhase" ];
@@ -121,6 +121,7 @@ in
       nix-prefetch-git
       nodejs_20
       # # P
+      pdfcpu
       podman-compose
       poppler
       # # S
@@ -210,6 +211,7 @@ in
       set -gx NODE_OPTIONS --max-old-space-size=8192
     '';
     shellInitLast = ''
+      # Use tmux by default
       if status is-interactive
         and not set -q TMUX
           exec tmux new-session -A -s Main
@@ -277,7 +279,6 @@ in
     btop = {
       enable = true;
       settings = {
-        color_theme = "adapta";
         show_battery = false;
         temp_scale = "celsius";
         update_ms = 1000;
@@ -769,14 +770,26 @@ in
           "*/.angular/*",
           "*/.git/*",
         })
+        -- Filetype
+        local function set_filetype_conf()
+          vim.bo.filetype = "conf"
+        end
+        local function set_filetype_dotenv()
+          vim.bo.filetype = "dotenv"
+        end
+        vim.api.nvim_create_augroup("FiletypeConfig", { clear = true })
+        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+          pattern = { "*/config", "*/conf" },
+          callback = set_filetype_conf,
+          group = "FiletypeConfig",
+        })
+        vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+          pattern = { "*/.env*" },
+          callback = set_filetype_dotenv,
+          group = "FiletypeConfig",
+        })
+        --
         vim.opt.clipboard = "unnamedplus"
-        -- Cursor
-        vim.opt.guicursor = {
-          "n-v-c:block-Cursor/lCursor",
-          "i-ci-ve:ver25-Cursor/lCursor",
-          "r-cr:hor20",
-          "o:hor50",
-        }
         -- Number
         vim.opt.nu = true
         vim.opt.cursorline = true
@@ -805,13 +818,13 @@ in
         vim.opt.termguicolors = false
         vim.opt.updatetime = 50
         --
-        local options = { noremap = true, silent = true }
         vim.g.mapleader = " "
+        local options = { noremap = true, silent = true }
         vim.keymap.set("n", "<C-z>", "<cmd>undo<CR>")
         vim.keymap.set("n", "<C-y>", "<cmd>redo<CR>")
         vim.keymap.set("n", "<leader>ee", function() vim.cmd("Ex") end)
         vim.keymap.set("n", "<leader>qa", function() vim.cmd("qa!") end)
-        -- Yank/Paste/Delete
+        -- Copy/Paste
         vim.keymap.set("x", "<leader>p", [["_dP]])
         vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
         vim.keymap.set("n", "<leader>Y", [["+Y]])
@@ -994,6 +1007,10 @@ in
         # Pane
         set -g pane-active-border "fg=#615296"
         set -ag pane-active-border bg=default
+        #
+        set -g allow-passthrough all
+        set -ag update-environment TERM
+        set -ag update-environment TERM_PROGRAM
       '';
       shell = "${config.home.profileDirectory}/bin/fish";
     };
@@ -1029,7 +1046,7 @@ in
         filetype = {
           rules = [
             { name = "*"; fg = "#ffffff"; }
-            { name = "*/"; fg = "#526596"; }
+            { name = "*/"; fg = "#615296"; }
           ];
         };
         icon = {
@@ -1053,7 +1070,19 @@ in
         require("no-status"):setup()
         -- Mime-ext
         require("mime-ext"):setup{
-          fallback_file1 = false,
+          with_files = {
+            config = "text/plain",
+            ["Dockerfile"] = "text/plain",
+            [".dockerignore"] = "text/plain",
+            [".gcloudignore"] = "text/plain",
+            [".gitignores"] = "text/plain",
+            [".gitattributes"] = "text/plain",
+            [".env"] = "text/plain",
+            [".env.development"] = "text/plain",
+            [".env.local"] = "text/plain",
+            [".env.production"] = "text/plain",
+            [".env.test"] = "text/plain",
+          },
         }
       '';
     };
